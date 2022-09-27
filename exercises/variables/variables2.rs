a65e5ac01cdf3835824771a3f7b0fb27703134a7
Michael Werle <Micha.Werle@sony.com>
2022-09-27 10:19:04 +0900

    variables/variables2

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,28 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+// MW - Rust variable types
+// integers : i/u 8-128bit/arch; eg i8, u32, isize
+//   123 - dec, 0xff - hex; 0o77 - octal, 0b0011 - binary, byte (u8) b'M'
+//   default: i32
+// float : f32, f64
+//   2.0, 
+// boolean : bool; true / false
+// character : (4-byte unicode) char; 'a', 'Z'
+// tuples... () : let t : (i32, f32, char) = (42, 7.35, 'c');
+//  t.0, t.1, ...
+// arrays [] : let a: [i32, 5] = [...]; (type, number);
+//  a[0], a[1], ...
+//
+
 
 fn main() {
-    let x;
+    //let x;
+    let x = 10;
+    //let x:i32 = 42;
+    //let x = "What do you get if you multiply six by seven?";
+    //let x = 1.4;
     if x == 10 {
         println!("x is ten!");
     } else {
