2a4203fc692a42ba7e3f9577ad8c3b3653e6a8ec
Michael Werle <Micha.Werle@sony.com>
2022-09-27 10:18:38 +0900

    variables/variables1

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,9 +2,15 @@
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+// MW: To declare variables in Rust:
+//   let <varname> = <value>; // Auto-typed variable
+//   let <varname>:<vartype> = <value>; // Explicitly-typed variable
+// "Variables" are, by default, immutable; to allow changing the variable, prefix with "mut" :
+//   let mut <varname>:<vartype> = <value>;
+//
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
